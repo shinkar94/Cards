3eb725cf89b30e73696d34e3137eef516970d28d
shinkar94 <romka94shinkarenko@gmail.com>
2023-07-15 11:26:44 +0300

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
