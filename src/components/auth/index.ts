da20d9218e8cf7a7aee3b975bde0c4f23a8d98ab
shinkar94 <romka94shinkarenko@gmail.com>
2023-07-30 16:33:38 +0300

    feat: added sign in and ControledCheckBox.tsx

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,1 +1,2 @@
 export * from './login-form/index.ts'
+export * from './sign-in/index.ts'
---
+++ src/components/auth/sign-in/sign-in.tsx
@@ -0,0 +1,68 @@
+import { Typography } from '../../ui/typography'
+import { TextField } from '../../ui/textfield'
+import { Button } from '../../ui/button'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { ControlledCheckbox } from '../../ui/ControledCheckBox'
+import s from './sign-in.module.scss'
+import { Card } from '../../ui/card'
+
+const sigInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(3),
+  rememberMe: z.boolean().default(false),
+})
+
+type SignInFormShem = z.infer<typeof sigInSchema>
+export const SignIn = () => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignInFormShem>({
+    resolver: zodResolver(sigInSchema),
+  })
+  const onSubmit = (data: SignInFormShem) => {
+    console.log(data)
+  }
+  return (
+    <Card className={s.signBlock}>
+      <Typography variant={'large'}>Sign In</Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          {...register('email')}
+          errorMessage={errors.email?.message}
+          label={'email'}
+          type={'default'}
+        />
+        <TextField
+          {...register('password')}
+          errorMessage={errors.password?.message}
+          label={'password'}
+          type={'default'}
+        />
+        <ControlledCheckbox
+          control={control}
+          name={'rememberMe'}
+          variant={'withText'}
+          checkBoxText={'remember me'}
+        />
+        <div className={s.forgotWrapper}>
+          <Button as={'a'} variant={'link'}>
+            <Typography variant={'body2'}>Forgot Password?</Typography>
+          </Button>
+        </div>
+
+        <Button fullWidth={true} className={s.submit} type="submit">
+          Sign In
+        </Button>
+      </form>
+      <Typography variant={'body2'}>Already have an account?</Typography>
+      <Button as={'a'} variant={'link'} className={s.signUp}>
+        Sign Up
+      </Button>
+    </Card>
+  )
+}
