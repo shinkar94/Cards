b0ed2b731424db73028b27bb6fed85556a805f74
shinkar94 <romka94shinkarenko@gmail.com>
2023-08-03 23:17:32 +0300

    feat: added page-list

---
+++ src/components/page/packs-list/packs-list.tsx
@@ -0,0 +1,107 @@
+const PacksList = () => {
+  return <div></div>
+}
+
+export default PacksList
+
+// import { useState } from 'react'
+//
+// import { ArrowDown, ArrowUp, Edit, Play, Trash } from '../../../assets'
+// import { Button, SliderDemo, TableElement, TabSwitcher, TextField, Typography } from '../../ui'
+//
+// import s from './packs-list.module.scss'
+//
+// type TypeTestData = {
+//   id: number
+//   name: string
+//   cardsNumber: number
+//   lastDate: string
+//   createdBy: string
+// }
+// export const PacksList = () => {
+//   const tabSwitcherOptions = [
+//     { id: 1, value: 'My Cards' },
+//     { id: 2, value: 'All Cards' },
+//   ]
+//
+//   const testData: TypeTestData[] = [
+//     { id: 1, name: 'Pack Name', cardsNumber: 4, lastDate: '24.07.2023', createdBy: 'Ivan Ivanov' },
+//     { id: 2, name: 'Pack Name', cardsNumber: 4, lastDate: '25.07.2023', createdBy: 'Ivan Ivanov' },
+//     { id: 3, name: 'Pack Name', cardsNumber: 4, lastDate: '26.07.2023', createdBy: 'Ivan Ivanov' },
+//     { id: 4, name: 'Pack Name', cardsNumber: 4, lastDate: '27.07.2023', createdBy: 'Ivan Ivanov' },
+//     { id: 5, name: 'Pack Name', cardsNumber: 4, lastDate: '28.07.2023', createdBy: 'Ivan Ivanov' },
+//     { id: 6, name: 'Pack Name', cardsNumber: 4, lastDate: '29.07.2023', createdBy: 'Ivan Ivanov' },
+//     { id: 7, name: 'Pack Name', cardsNumber: 4, lastDate: '30.07.2023', createdBy: 'Ivan Ivanov' },
+//   ]
+//
+//   const [sortTable, setSortTable] = useState(false)
+//   const changeSort = (status: boolean) => setSortTable(status)
+//
+//   return (
+//     <div className={s.packListBlock}>
+//       <div className={s.headBlock}>
+//         <Typography variant={'large'}>Packs list</Typography>
+//         <Button variant={'primary'}>Add New Pack</Button>
+//       </div>
+//       <div className={s.settingsBlock}>
+//         <TextField type={'searchType'} className={s.textField} />
+//         <div>
+//           <Typography variant={'body2'} className={s.titleSettings}>
+//             Show packs cards
+//           </Typography>
+//           <TabSwitcher
+//             onChangeCallback={() => {}}
+//             options={tabSwitcherOptions}
+//             classname={s.switcher}
+//           />
+//         </div>
+//         <div>
+//           <Typography variant={'body2'} className={s.titleSettings}>
+//             Show packs cards
+//           </Typography>
+//           <SliderDemo minValue={0} maxValue={10} />
+//         </div>
+//         <Button variant={'secondary'}>
+//           <Trash />
+//           Clear Filter
+//         </Button>
+//       </div>
+//       <TableElement.Root>
+//         <TableElement.Head>
+//           <TableElement.Row>
+//             <TableElement.HeadCell>Name</TableElement.HeadCell>
+//             <TableElement.HeadCell>Cards</TableElement.HeadCell>
+//             <TableElement.HeadCell
+//               onClick={() => {
+//                 changeSort(!sortTable)
+//               }}
+//             >
+//               Last Updated {sortTable ? <ArrowDown /> : <ArrowUp />}
+//             </TableElement.HeadCell>
+//             <TableElement.HeadCell>Created by</TableElement.HeadCell>
+//             <TableElement.HeadCell></TableElement.HeadCell>
+//           </TableElement.Row>
+//         </TableElement.Head>
+//         <TableElement.Body>
+//           {testData.map(el => {
+//             return (
+//               <TableElement.Row key={el.id}>
+//                 <TableElement.Cell>{el.name}</TableElement.Cell>
+//                 <TableElement.Cell>{el.cardsNumber}</TableElement.Cell>
+//                 <TableElement.Cell>{el.lastDate}</TableElement.Cell>
+//                 <TableElement.Cell>{el.createdBy}</TableElement.Cell>
+//                 <TableElement.Cell>
+//                   <div className={s.icons}>
+//                     <Play />
+//                     <Edit />
+//                     <Trash />
+//                   </div>
+//                 </TableElement.Cell>
+//               </TableElement.Row>
+//             )
+//           })}
+//         </TableElement.Body>
+//       </TableElement.Root>
+//     </div>
+//   )
+// }
