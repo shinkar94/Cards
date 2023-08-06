7e3d9bdcbfb48b86fe20b70b8bc0c7b54f0f0f9d
shinkar94 <romka94shinkarenko@gmail.com>
2023-08-06 19:05:34 +0300

    refactor: refactor main and router

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
+
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
-import { App } from './App.tsx'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/700.css'
-import './styles/index.scss'
-import { BrowserRouter } from 'react-router-dom'
+import { App } from './App'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 )
